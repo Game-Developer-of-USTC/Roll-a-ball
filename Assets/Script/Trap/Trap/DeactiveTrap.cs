71e22c82abc3a0940ef13e90477760f74911bd21
When <balanceuhen@gmail.com>
2020-12-15 20:31:32 +0800

    完成消失机关的制作

---
+++ Assets/Script/Trap/Trap/DeactiveTrap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactiveTrap : MonoBehaviour, TriggerTrap
+{
+	public void trapTrigger()
+	{
+		gameObject.SetActive(false);
+	}
+	public void trapInit()
+	{
+		// Do no thing
+	}
+}
