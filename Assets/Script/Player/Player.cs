2bd5bdec5cce2c251f5dd82fac65ccfc1a156f3e
When <balanceuhen@gmail.com>
2020-12-21 15:02:14 +0800

    更新样例关卡

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,97 +5,118 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-	public Rigidbody2D rb;
-	public LayerMask ground;
-	public List<PhysicsMaterial2D> material2Ds;
-	public List<int> masses;
-	public List<int> gravity;
-	public List<float> grab;
-	public float force;
-	public float jumpForce;
-	public float dis;
-	private bool jumpPressed;
-	private bool isJumping;
-	public float acclerationCoef;
+    [Header("辅助参数")]
+    public Rigidbody2D rb;
+    public LayerMask ground;
+    [Header("三个球的参数")]
+    public List<PhysicsMaterial2D> material2Ds;
+    public List<float> masses;
+    public List<float> gravity;
+    public List<float> grab;
+    [Header("跳跃参数")]
+    public float moveForce;
+    public float jumpForce;
+    public float jumpHeld;
+    public float jumpDuration;
+    public float jumpTime;
+    [Header("状态参数")]
+    public float dis;
+    public bool isJumpPressed;
+    public bool isJumping;
+    public bool isJumpHeld;
+    private bool isGround;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBall(1);
+    }
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		SwitchBall(1);
-		isJumping = true;
-	}
+    // 更新状态
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) SwitchBall(0);
+        else if (Input.GetKeyDown(KeyCode.W)) SwitchBall(1);
+        else if (Input.GetKeyDown(KeyCode.E)) SwitchBall(2);
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Q)) SwitchBall(0);
-		else if (Input.GetKeyDown(KeyCode.W)) SwitchBall(1);
-		else if (Input.GetKeyDown(KeyCode.E)) SwitchBall(2);
+        if (Input.GetButtonDown("Jump"))
+            isJumpPressed = true;
+        isJumpHeld = Input.GetButton("Jump");
+        isGround = isOnGround();
+    }
 
-		if (Input.GetButtonDown("Jump"))
-			jumpPressed = true;
-	}
+    void FixedUpdate()
+    {
+        horizontalMove();
+        jumpMove();
+    }
 
-	void FixedUpdate()
-	{
-		float accleration = Input.GetAxis("Horizontal");
-		// float F = accleration * Force - sgn(rb.velocity.x) * rb.mass * rb.sharedMaterial.friction;
-		// rb.velocity = new Vector2(rb.velocity.x + F / rb.mass * Time.deltaTime, rb.velocity.y);
-		rb.AddRelativeForce(new Vector2(accleration * force, 0), ForceMode2D.Force);
+    void horizontalMove()
+    {
+        float accleration = Input.GetAxis("Horizontal");
+        // float F = accleration * Force - sgn(rb.velocity.x) * rb.mass * rb.sharedMaterial.friction;
+        // rb.velocity = new Vector2(rb.velocity.x + F / rb.mass * Time.deltaTime, rb.velocity.y);
+        rb.AddRelativeForce(new Vector2(accleration * moveForce, 0), ForceMode2D.Force);
+    }
 
-		if (isOnGround())
-			isJumping = false;
-		if (isNearWall())
-			isJumping = false;
-		if (jumpPressed && isJumping == false)
-		{
-			// rb.velocity = new Vector2(rb.velocity.x, jumpForce / rb.mass * Time.deltaTime);
-			rb.AddRelativeForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-			jumpPressed = false;
-			isJumping = true;
-		}
-		jumpPressed = false;
-	}
-	int sgn(float x)
-	{
-		if (x < 0) return -1;
-		if (x > 0) return 1;
-		return 0;
-	}
+    void jumpMove()
+    {
+        if (isJumpPressed && isJumping == false && isGround)
+        {
+            // rb.velocity = new Vector2(rb.velocity.x, jumpForce / rb.mass * Time.deltaTime);
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpTime = Time.time + jumpDuration;
+            isJumping = true;
+            isJumpPressed = false;
+        }
+        else if (isJumping)
+        {
+            if (isJumpHeld)
+                rb.AddForce(new Vector2(0f, jumpHeld), ForceMode2D.Impulse);
+            if (jumpTime < Time.time)
+                isJumping = false;
+        }
+    }
 
-	void SwitchBall(int ball)
-	{
-		rb.mass = masses[ball];
-		rb.gravityScale = gravity[ball];
-		switchMaterial(material2Ds[ball]);
-	}
+    int sgn(float x)
+    {
+        if (x < 0) return -1;
+        if (x > 0) return 1;
+        return 0;
+    }
 
-	void switchMaterial(PhysicsMaterial2D m)
-	{
-		rb.sharedMaterial = m;
-	}
+    void SwitchBall(int ball)
+    {
+        rb.mass = masses[ball];
+        rb.gravityScale = gravity[ball];
+        switchMaterial(material2Ds[ball]);
+    }
 
-	public void Death()
-	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-	}
+    void switchMaterial(PhysicsMaterial2D m)
+    {
+        rb.sharedMaterial = m;
+    }
 
-	bool isOnGround()
-	{
-		RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, dis, ground);
-		Debug.DrawRay(transform.position, Vector2.down * dis, Color.green, 1);
-		return ray;
-	}
+    public void Death()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
-	bool isNearWall()
-	{
-		RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, dis, ground);
-		Debug.DrawRay(transform.position, Vector2.right * dis, Color.green, 1);
+    bool isOnGround()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, dis, ground);
+        Debug.DrawRay(transform.position, Vector2.down * dis, Color.green, 1);
+        return ray;
+    }
 
-		RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, dis, ground);
-		Debug.DrawRay(transform.position, Vector2.left * dis, Color.green, 1);
+    bool isNearWall()
+    {
+        RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, dis, ground);
+        Debug.DrawRay(transform.position, Vector2.right * dis, Color.green, 1);
 
-		return right || left;
-	}
+        RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, dis, ground);
+        Debug.DrawRay(transform.position, Vector2.left * dis, Color.green, 1);
+
+        return right || left;
+    }
 }
---
+++ Assets/Script/Trap/Trap/FallTrap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTrap : MonoBehaviour, TriggerTrap
+{
+    // Start is called before the first frame update
+    public float waitTime;
+    public RigidbodyType2D initType = RigidbodyType2D.Static;
+    public RigidbodyType2D afterType = RigidbodyType2D.Kinematic;
+    public void trapInit()
+    {
+        this.GetComponent<Rigidbody2D>().bodyType = initType;
+    }
+
+    public void trapTrigger()
+    {
+        Invoke("changeType", waitTime);
+    }
+
+    void changeType()
+    {
+        this.GetComponent<Rigidbody2D>().bodyType = afterType;
+    }
+}
