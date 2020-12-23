03c4523021da166f96b2c6e27b51960d3ccbeae8
When <balanceuhen@gmail.com>
2020-12-24 00:29:00 +0800

    完成一个机关

---
+++ Assets/Script/Special/level01/Trap1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Trap1 : MonoBehaviour
+{
+    public GameObject player;
+    public LayerMask playerLayer;
+    public float yOffset;
+    void Awake()
+    {
+        player = GameObject.Find("/Player");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerBelow())
+        {
+            transform.position = new Vector2(transform.position.x,
+            Math.Max(transform.position.y, player.transform.position.y + yOffset));
+        }
+    }
+
+    bool isPlayerBelow()
+    {
+        RaycastHit2D right = Physics2D.Raycast(
+            new Vector2(transform.position.x + 0.5f, transform.position.y),
+            Vector2.down,
+            yOffset + 1,
+            playerLayer);
+        RaycastHit2D left = Physics2D.Raycast(
+            new Vector2(transform.position.x - 0.5f, transform.position.y),
+            Vector2.down,
+            yOffset + 1,
+            playerLayer);
+
+        return right || left;
+    }
+}
