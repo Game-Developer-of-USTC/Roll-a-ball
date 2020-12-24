90c90237fc958f836a3f4dbb85a82383f229760d
When <balanceuhen@gmail.com>
2020-12-24 22:57:02 +0800

    左上角得分板

---
+++ Assets/Script/Special/level01/AddScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class AddScore : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Collection")
+        {
+            Destroy(other.gameObject);
+            int now = Int32.Parse(text.text);
+            now = now + 1;
+            text.text = now + "";
+        }
+    }
+}
