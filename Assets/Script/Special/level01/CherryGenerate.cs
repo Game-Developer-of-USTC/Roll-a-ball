98a5b14cfce5bffcde158150983fe41c200fd87f
When <balanceuhen@gmail.com>
2020-12-24 23:13:21 +0800

    备份

---
+++ Assets/Script/Special/level01/CherryGenerate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryGenerate : MonoBehaviour
+{
+    public float deltaTime;
+    public Vector2 rightdown;
+    public Vector2 leftup;
+    public float regenerateTime;
+    public int generateNum;
+    public GameObject cherry;
+
+    private void FixedUpdate()
+    {
+
+    }
+}
