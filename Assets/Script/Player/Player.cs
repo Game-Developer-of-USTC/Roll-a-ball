b91760d1586eb41710636029e587996bda41bba8
When <balanceuhen@gmail.com>
2020-12-24 12:24:18 +0800

    打磨

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -122,9 +122,6 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Enemy")
-        {
-            this.Death();
-        }
+
     }
 }
--- Assets/Script/Special/level01/Enemy1.cs
+++ Assets/Script/Special/level01/Enemy1.cs
@@ -36,5 +36,52 @@ public class Enemy1 : MonoBehaviour
             rayDis,
             groundLayer);
         if (face) velocity = -velocity;
+
+        Debug.DrawRay(
+            transform.position + new Vector3(-0.5f, 0.5f, 0),
+            10 * Vector2.up,
+            Color.green
+        );
+
+        Debug.DrawRay(
+            transform.position + new Vector3(+0.5f, 0.5f, 0),
+            10 * Vector2.up,
+            Color.green
+        );
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            RaycastHit2D left = Physics2D.Raycast(
+            transform.position + new Vector3(-0.5f, 0.5f, 0),
+            Vector2.up,
+            10);
+
+            if (left) Debug.Log(left.collider.tag);
+            else Debug.Log("Found nothing left");
+            if (left && left.collider.tag == "Player")
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            RaycastHit2D right = Physics2D.Raycast(
+            transform.position + new Vector3(0.5f, 0.5f, 0),
+            Vector2.up,
+            10);
+
+            if (right)
+                Debug.Log(right.collider.tag);
+            else Debug.Log("Found nothing right");
+            if (right && right.collider.tag == "Player")
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            other.gameObject.GetComponent<Player>().Death();
+        }
     }
 }
