cb7758d56912dd4f412f50c5935208a4d3afc4eb
When <balanceuhen@gmail.com>
2020-12-24 12:41:57 +0800

    修复bug

--- Assets/Script/Special/level01/Trap2.cs
+++ Assets/Script/Special/level01/Trap2.cs
@@ -14,12 +14,45 @@ public class Trap2 : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        Debug.Log("Collided");
         if (other.gameObject.tag == "Player")
         {
-            if (player.transform.position.y < transform.position.y)
+            // if (checkPlayerBelow())
             {
-                enemy.SetActive(true);
+                if (enemy)
+                    enemy.SetActive(true);
             }
         }
+    }
+
+    public bool checkPlayerBelow()
+    {
+        RaycastHit2D left = Physics2D.Raycast(
+        transform.position + new Vector3(-0.5f, 0, 0),
+        Vector2.down,
+        10);
+
+        RaycastHit2D right = Physics2D.Raycast(
+        transform.position + new Vector3(+0.5f, 0, 0),
+        Vector2.down,
+        10);
+
+        Debug.Log(left);
+        Debug.Log(right);
+        return (left && left.collider.tag == "Player") ||
+                (right && right.collider.tag == "Player");
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(
+            transform.position + new Vector3(-0.5f, 0, 0),
+            10 * Vector2.down
+        );
+
+        Debug.DrawRay(
+            transform.position + new Vector3(+0.5f, 0, 0),
+            10 * Vector2.down
+        );
     }
 }
