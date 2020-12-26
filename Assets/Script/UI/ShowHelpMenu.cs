ce1831fc6226fc26158296942ce4e1fc87c86adf
When <balanceuhen@gmail.com>
2020-12-26 20:13:25 +0800

    添加帮助信息

---
+++ Assets/Script/UI/ShowHelpMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHelpMenu : MonoBehaviour
+{
+    public GameObject go;
+    public void show()
+    {
+        go.SetActive(true);
+    }
+    public void hide()
+    {
+        go.SetActive(false);
+    }
+}
