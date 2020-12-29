bf64c51c0159c641e6b2a6d36ab3e9d9e1751ec0
dyxx <skiper@mail.ustc.edu.cn>
2020-12-29 20:21:26 +0800

    11111

--- Assets/Script/UI/HelpButton.cs
+++ Assets/Script/UI/HelpButton.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 public class HelpButton : MonoBehaviour
 {
     public GameObject mainMenu;
-    public GameObject selectMenu;
+    public GameObject helpPanel;
     public void help()
     {
-        SceneManager.LoadScene(SceneManager.GetSceneByName("Help").buildIndex);
-        // selectMenu.SetActive(true);
-        // mainMenu.SetActive(false);
+        //SceneManager.LoadScene(SceneManager.GetSceneByName("Help").buildIndex);
+        helpPanel.SetActive(true);
+        mainMenu.SetActive(false);
     }
 }
