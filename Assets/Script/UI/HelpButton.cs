a099b063770fd8a101a1775066197146fb766d44
dyxx <skiper@mail.ustc.edu.cn>
2020-12-29 19:22:09 +0800

    更新了Help

---
+++ Assets/Script/UI/HelpButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HelpButton : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject selectMenu;
+    public void help()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName("Help").buildIndex);
+        // selectMenu.SetActive(true);
+        // mainMenu.SetActive(false);
+    }
+}
--- Assets/Script/UI/MainMenu.cs
+++ Assets/Script/UI/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+//        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Quit()
